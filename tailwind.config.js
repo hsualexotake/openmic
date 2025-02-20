4e94ecc1979e9173bdbaa66f979515ab8474a7fb
hsualexotake <hsualexotake@gmail.com>
2025-02-19 20:29:25 -0500

    fixed tailwindcss problem

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure JSX files are included
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
